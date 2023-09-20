7cc44232a0c6c1b8f48256e9e78b11e5ce16074a
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-20 16:42:45 +0700

    Initial Commit

---
+++ CountryFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryFacts
+//
+//  Created by Yuga Samuel on 20/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
