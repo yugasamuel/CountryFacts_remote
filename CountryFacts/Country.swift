f82047814588d9d84339764de5ff6f84caa9ce57
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-20 17:36:14 +0700

    Add country model

---
+++ CountryFacts/Country.swift
@@ -0,0 +1,17 @@
+//
+//  Country.swift
+//  CountryFacts
+//
+//  Created by Yuga Samuel on 20/09/23.
+//
+
+import Foundation
+
+class Country: Codable {
+    let id: String
+    let name: String
+    let capital: String
+    let size: Double
+    let population: Int
+    let currency: String
+}
