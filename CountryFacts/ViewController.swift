6c31b0eb153b044476164d25ed0c551aa510eba4
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-20 19:12:47 +0700

    Add data to countries table view

--- CountryFacts/ViewController.swift
+++ CountryFacts/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var countries: [String: Country] = Bundle.main.decode("countries.json")
+    var countriesDict: [String: Country] = Bundle.main.decode("countries.json")
+    var countries = [Country]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadCountries()
+    }
+    
+    func loadCountries() {
+        for key in countriesDict.keys.sorted() {
+            countries.append(countriesDict[key]!)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = countries[indexPath.row].name
+        
+        cell.contentConfiguration = content
+        
+        return cell
     }
 }
 
